15d70bb9b289c69aa42cdf1dc9046e1489c60009
DNofulla <dnofulla@outlook.com>
2022-02-28 00:07:19 -0500

    Added Twilio Controller

--- server/src/main/java/daniel/nofulla/customsearchenginebackend/HelloWorldController.java
+++ server/src/main/java/daniel/nofulla/customsearchenginebackend/HelloWorldController.java
@@ -1,14 +1,15 @@
 package daniel.nofulla.customsearchenginebackend;
 
 import net.minidev.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/")
+    @GetMapping()
     public JSONObject helloWorld() {
-        return new JSONObject().appendField("message", "Hello World from Spring Boot");
+        return new JSONObject().appendField("message", "Welcome to the custom search engine API");
     }
 }
